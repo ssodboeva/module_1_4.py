bbfda89d1640c8ec312a5d5e2f50a481ef70f22d
ssodboeva <radnaevas87@gmail.com>
2024-10-01 00:29:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+my_string = input ('Как Вас зовут? ')
+print (len (my_string))
+print (my_string.upper ())
+print (my_string.lower ())
+my_string.replace (' ', '')
+print (my_string [0])
+print (my_string [-1])
